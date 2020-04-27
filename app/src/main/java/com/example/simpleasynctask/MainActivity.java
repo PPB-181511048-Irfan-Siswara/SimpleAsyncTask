50d7198a0dcaada6c98e208a65a314b13ff5f09a
irfanswr <irfan.siswara.tif18@polban.ac.id>
2020-04-27 15:02:47 +0700

    Challenge

--- app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -4,47 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextView;
+    private ProgressBar mProgressBar;
     private static final String TEXT_STATE = "currentText";
+    private static final String PROGRESS_STATE = "currentProgress";
 
-    private TextView mTextView = null;
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE,
+                mTextView.getText().toString());
+        outState.putInt(PROGRESS_STATE,
+                mProgressBar.getProgress());
+    }
 
-    /**
-     * Initializes the activity.
-     * @param savedInstanceState The current state data
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.textView1);
+        mTextView = findViewById(R.id.textView1);
+        mProgressBar = findViewById(R.id.progressBar);
 
         if(savedInstanceState!=null){
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+            mProgressBar.setProgress(savedInstanceState.getInt(PROGRESS_STATE));
         }
     }
 
-    /**`
-     * Handles the onCLick for the "Start Task" button. Launches the AsyncTask
-     * which performs work off of the UI thread.
-     *
-     * @param view The view (Button) that was clicked.
-     */
-    public void startTask (View view) {
-
+    public void startTask(View view) {
         mTextView.setText(R.string.napping);
-
-        new SimpleAsyncTask(mTextView).execute();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putString(TEXT_STATE, mTextView.getText().toString());
+        new SimpleAsyncTask(mTextView, mProgressBar).execute();
     }
 }
